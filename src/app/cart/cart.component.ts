7eaf6cc3fd6e813b00406ddddd1ebe0fa1e6044f
scamer300 <115151242+scamer300@users.noreply.github.com>
2023-08-23 16:42:32 +0100

    5 files modified, 1 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,5 +10,16 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({ name: '', address: '' });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    window.alert('Your order has been submitted');
+    this.checkoutForm.reset();
+  }
 }
